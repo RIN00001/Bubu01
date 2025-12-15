8019f2995f013b8c79ab4f450084b6c665939bbe
Vincent <vincent01@student.ciputra.ac.id>
2025-12-15 09:02:35 +0700

    Menambah Rute melihat semua saving

--- src/routes/private-api.ts
+++ src/routes/private-api.ts
@@ -35,6 +35,7 @@ privateRouter.put("/items/:itemId", ItemController.update);
 privateRouter.delete("/items/:itemId", ItemController.remove);
 
 //Saving Routes
+privateRouter.get("/savings", SavingController.getAllSavings);
 privateRouter.post("/saving", SavingController.createSaving);
 privateRouter.get("/saving/:id", SavingController.getSavingById);
 privateRouter.put("/saving/:id", SavingController.updateSaving);
